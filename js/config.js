f006f9abeb72d7705569bff85b91f77bf78498ee
DaveL17 <dave.lehrer@icloud.com>
2025-12-15 12:46:46 -0600

    Adds message classes containing 'failure' to the error class coloring.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //=============================================================================
 // Common config values
 //=============================================================================
-const APP_VERSION = "Beta 6";
+const APP_VERSION = "Beta 7";
 const APP_NAME = "Indigo Log Viewer"
 const AUTHOR = "DaveL17";
