9890f79cdac2b0aeb1d9d0a3679295a15870e42d
DaveL17 <dave.lehrer@icloud.com>
2026-05-06 06:28:08 -0500

    Bump version to 2026.5.1

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //=============================================================================
 // Common config values
 //=============================================================================
-const APP_VERSION = "2026.2.6";
+const APP_VERSION = "2026.5.1";
 const APP_NAME = "Indigo Log Viewer";
 const AUTHOR = "DaveL17";
